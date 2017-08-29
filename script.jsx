892905b361e7bfb91cebb5ca8ceaf5e6b1b3615c
ryan <ryanc@cnyes.com>
2017-08-29 18:00:11 +0800

    update script.jsx

---
+++ script.jsx
@@ -0,0 +1,11 @@
+
+var Game=React.createClass({
+   render:function(){
+       <div className='game'>
+           <div className='info'>
+               <div></div>
+               <ol></ol>
+           </div>
+       </div>
+   }
+});
